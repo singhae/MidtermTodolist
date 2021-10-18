e9da488d02ff2be42bfe726e468baf52fea4ad42
singhae <thsgo1357@naver.com>
2021-10-18 15:21:04 +0900

    Change-Id: I0000000000000000000000000000000000000000 2
    Change-Id: I8a6b1e20b766bcd740e5a00031b927a13634f440

--- MidtertmTodolist/src/com/todo/TodoMain.java
+++ MidtertmTodolist/src/com/todo/TodoMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import com.todo.dao.TodoList;
 import com.todo.menu.Menu;
 import com.todo.service.TodoUtil;
-
+ //
 public class TodoMain {
 	
 	public static void start() {
