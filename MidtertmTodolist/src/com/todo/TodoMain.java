8eccb2e3beca4ae2e99f95c94418be4282d6894b
singhae <thsgo1357@naver.com>
2021-10-18 15:30:24 +0900

    2nd Change-Id: Iff0f639399d4589aee9cd5bdfcddcdb873d925f8

--- MidtertmTodolist/src/com/todo/TodoMain.java
+++ MidtertmTodolist/src/com/todo/TodoMain.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 import com.todo.dao.TodoList;
 import com.todo.menu.Menu;
 import com.todo.service.TodoUtil;
- //
+ //s
 public class TodoMain {
 	
 	public static void start() {
 	
-		Scanner sc = new Scanner(System.in); 
+		Scanner sc = new Scanner(System.in);  //
 		TodoList l = new TodoList();  
 		//l.importData("todolist.txt");
 		
